6ccd939c95081c1de2f1209a28539da3f8a74ed9
Daniel Schultz <d.schultz@phytec.de>
2021-05-17 22:26:55 +0200

    hardware: soc: Add I2C to Carbon1
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- hardware/scala/zibal/soc/Carbon1.scala
+++ hardware/scala/zibal/soc/Carbon1.scala
@@ -21,6 +21,7 @@ import zibal.peripherals.io.gpio.{Apb3Gpio, Gpio, GpioCtrl}
 import zibal.peripherals.misc.mtimer.{Apb3MachineTimer, MachineTimerCtrl}
 import zibal.peripherals.com.uart.{Apb3Uart, Uart, UartCtrl}
 import zibal.peripherals.com.spi.{Apb3SpiMaster, Spi, SpiCtrl}
+import zibal.peripherals.com.i2c.{Apb3I2cController, I2c, I2cCtrl}
 
 
 object Carbon1 {
@@ -36,17 +37,19 @@ object Carbon1 {
   case class Peripherals (
     uartStd: UartCtrl.Parameter,
     gpioStatus: GpioCtrl.Parameter,
-    spi0: SpiCtrl.Parameter
+    spi0: SpiCtrl.Parameter,
+    i2c0: I2cCtrl.Parameter
   ) {}
 
   object Peripherals {
     def default = Carbon.Parameter.light(
       Peripherals(
-        uartStd = UartCtrl.Parameter.lightweight,
+        uartStd = UartCtrl.Parameter.default,
         gpioStatus = GpioCtrl.Parameter(4, 2, (0 to 2), (3 to 3), (3 to 3)),
-        spi0 = SpiCtrl.Parameter.lightweight
+        spi0 = SpiCtrl.Parameter.default,
+        i2c0 = I2cCtrl.Parameter.default
       ),
-      3
+      4
     )
   }
 
@@ -56,6 +59,7 @@ object Carbon1 {
       val uartStd = master(Uart.Io(peripherals.uartStd))
       val gpioStatus = Gpio.Io(peripherals.gpioStatus)
       val spi0 = master(Spi.Io(peripherals.spi0))
+      val i2c0 = master(I2c.Io(peripherals.i2c0))
     }
 
     val pers = new ClockingArea(clocks.systemClockDomain) {
@@ -74,6 +78,11 @@ object Carbon1 {
       system.apbMapping += spiMaster0Ctrl.io.bus -> (0x40000, 4 kB)
       spiMaster0Ctrl.io.spi <> io_per.spi0
       system.plicCtrl.io.sources(3) := spiMaster0Ctrl.io.interrupt
+
+      val i2cController0Ctrl = Apb3I2cController(peripherals.i2c0)
+      system.apbMapping += i2cController0Ctrl.io.bus -> (0x50000, 4 kB)
+      i2cController0Ctrl.io.i2c <> io_per.i2c0
+      system.plicCtrl.io.sources(4) := i2cController0Ctrl.io.interrupt
 
       val apbDecoder = Apb3Decoder(
         master = system.apbBridge.io.apb,
