3d4a42e03a97281bf77b4a9dc080ec05a638e7ea
Daniel Schultz <d.schultz@phytec.de>
2021-11-02 21:18:00 +0100

    fixup! hardware: Add XIlinx LVDS support

--- hardware/scala/zibal/blackboxes/xilinx/a7/IO.v
+++ hardware/scala/zibal/blackboxes/xilinx/a7/IO.v
@@ -86,6 +86,8 @@ module IBUFDS (
 );
 
 parameter CAPACITANCE = "DONT_CARE";
+parameter DIFF_TERM = "FALSE";
+parameter DQS_BIAS = "FALSE";
 parameter IBUF_DELAY_VALUE = "0";
 parameter IBUF_LOW_PWR = "TRUE";
 parameter IFD_DELAY_VALUE = "AUTO";
