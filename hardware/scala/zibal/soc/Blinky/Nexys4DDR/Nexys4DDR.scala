8500a8ba6ad51bf0b66cc2097985bdc7b29b0b25
Daniel Schultz <d.schultz@phytec.de>
2022-01-15 11:43:55 +0100

    Add Blinky
    This minimal design can be used as demo for FPGA and ASIC flows because
    it's easy and fast.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ hardware/scala/zibal/soc/Blinky/Nexys4DDR/Nexys4DDR.scala
@@ -0,0 +1,101 @@
+package zibal.soc.blinky
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib._
+
+import zibal.soc.Blinky
+import zibal.misc.{ElementsConfig, XilinxTools, SimulationHelper, TestCases}
+import zibal.blackboxes.xilinx.a7._
+
+
+object Nexys4DDRBoard {
+  def apply(source: String) = Nexys4DDRBoard(source)
+
+  def quartzFrequency = 100 MHz
+
+  def main(args: Array[String]) {
+    val elementsConfig = ElementsConfig(this)
+
+    val compiled = elementsConfig.genFPGASimConfig.compile {
+      val board = Nexys4DDRBoard(args(0))
+      board
+    }
+    args(1) match {
+      case "simulate" =>
+        compiled.doSimUntilVoid("simulate") { dut =>
+          val testCases = TestCases()
+          testCases.addClock(dut.io.clock, quartzFrequency, 1 ms)
+          testCases.addReset(dut.io.reset, 1 us)
+        }
+      case _ =>
+        println(s"Unknown simulation ${args(1)}")
+    }
+  }
+
+  case class Nexys4DDRBoard(source: String) extends Component {
+    val io = new Bundle {
+      val clock = inout(Analog(Bool))
+      val reset = inout(Analog(Bool))
+      val led = Vec(inout(Analog(Bool())), 6)
+    }
+
+    val top = Nexys4DDRTop()
+    val analogFalse = Analog(Bool)
+    analogFalse := False
+    val analogTrue = Analog(Bool)
+    analogTrue := True
+
+    top.io.clock.PAD := io.clock
+    top.io.reset.PAD := io.reset
+
+    for (index <- 0 until 6) {
+      io.led(index) <> top.io.led(index).PAD
+    }
+  }
+}
+
+
+object Nexys4DDRTop {
+  def apply() = Nexys4DDRTop()
+
+  def main(args: Array[String]) {
+    val elementsConfig = ElementsConfig(this)
+    val spinalConfig = elementsConfig.genFPGASpinalConfig
+
+    args(0) match {
+      case "prepare" =>
+        println("Nothing to do here!")
+      case _ =>
+        spinalConfig.generateVerilog({
+          val top = Nexys4DDRTop()
+          XilinxTools.Xdc(elementsConfig).generate(top.io)
+          top
+        })
+    }
+  }
+
+  case class Nexys4DDRTop() extends Component {
+    val io = new Bundle {
+      val clock = XilinxCmosIo("E3").clock(Nexys4DDRBoard.quartzFrequency)
+      val reset = XilinxCmosIo("C12")
+      val led = Vec(
+        XilinxCmosIo("H17"),
+        XilinxCmosIo("K15"),
+        XilinxCmosIo("J13"),
+        XilinxCmosIo("N14"),
+        XilinxCmosIo("R18"),
+        XilinxCmosIo("V17")
+      )
+    }
+
+    val soc = Blinky()
+
+    io.clock <> IBUF(soc.io.clock)
+    io.reset <> IBUF(soc.io.reset)
+
+    for (index <- 0 until 6) {
+      io.led(index) <> OBUF(soc.io.led(index))
+    }
+  }
+}
---
+++ hardware/scala/zibal/soc/Blinky/VirtualSG13S/VirtualSG13S.scala
@@ -0,0 +1,110 @@
+package zibal.soc.blinky
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib._
+
+import zibal.soc.Blinky
+import zibal.misc.{CadenceTools, SimulationHelper, ElementsConfig, TestCases}
+import zibal.blackboxes.ihp.sg13s._
+
+
+object VirtualSG13SBoard {
+  def apply(source: String) = VirtualSG13SBoard(source)
+
+  val quartzFrequency = 100 MHz
+
+  def main(args: Array[String]) {
+    val elementsConfig = ElementsConfig(this)
+    val spinalConfig = elementsConfig.genASICSpinalConfig
+
+    val compiled = elementsConfig.genASICSimConfig.compile {
+      val board = VirtualSG13SBoard(args(0))
+      board
+    }
+    args(1) match {
+      case "simulate" =>
+        compiled.doSimUntilVoid("simulate") { dut =>
+          val testCases = TestCases()
+          testCases.addClock(dut.io.clock, quartzFrequency, 3 ms)
+          testCases.addReset(dut.io.reset, 1 us)
+        }
+      case _ =>
+        println(s"Unknown simulation ${args(1)}")
+    }
+  }
+
+  case class VirtualSG13SBoard(source: String) extends Component {
+    val io = new Bundle {
+      val clock = inout(Analog(Bool))
+      val reset = inout(Analog(Bool))
+      val led = Vec(inout(Analog(Bool())), 6)
+    }
+    val clockPeriod = 10
+
+    val top = VirtualSG13STop()
+    val analogFalse = Analog(Bool)
+    analogFalse := False
+    val analogTrue = Analog(Bool)
+    analogTrue := True
+
+    top.io.clock.PAD := io.clock
+    top.io.reset.PAD := io.reset
+
+    for (index <- 0 until 6) {
+      io.led(index) <> top.io.led(index).PAD
+    }
+  }
+}
+
+
+object VirtualSG13STop {
+  def apply() = VirtualSG13STop()
+
+  def main(args: Array[String]) {
+    val elementsConfig = ElementsConfig(this)
+    val spinalConfig = elementsConfig.genASICSpinalConfig
+
+    args(0) match {
+      case "prepare" =>
+        println("Nothing to do here!")
+      case _ =>
+        spinalConfig.generateVerilog({
+          val top = VirtualSG13STop()
+          val io = CadenceTools.Io(elementsConfig)
+          io.addPad("top", 1, "gndpad")
+          io.addPad("right", 1, "gndcore")
+          io.addPad("bottom", 1, "vddcore")
+          io.addPad("left", 1, "vddpad")
+          io.addCorner("topright", 90, "corner")
+          io.addCorner("bottomright", 0, "corner")
+          io.addCorner("bottomleft", 270, "corner")
+          io.addCorner("topleft", 180, "corner")
+          io.generate(top.io, elementsConfig.zibalBuildPath)
+          val sdc = CadenceTools.Sdc(elementsConfig)
+          sdc.addClock(top.io.clock.PAD, VirtualSG13SBoard.quartzFrequency)
+          sdc.generate(elementsConfig.zibalBuildPath)
+          top
+        })
+    }
+  }
+
+  case class VirtualSG13STop() extends Component {
+    val io = new Bundle {
+      val clock = IhpCmosIo("top", 0)
+      val reset = IhpCmosIo("top", 2)
+      val led = Vec(IhpCmosIo("right", 0), IhpCmosIo("right", 2),
+                    IhpCmosIo("bottom", 0), IhpCmosIo("bottom", 2),
+                    IhpCmosIo("left", 0), IhpCmosIo("left", 2))
+    }
+
+    val soc = Blinky()
+
+    io.clock <> ixc013_i16x(soc.io.clock)
+    io.reset <> ixc013_i16x(soc.io.reset)
+
+    for (index <- 0 until 6) {
+      io.led(index) <> ixc013_b16m().asOutput(soc.io.led(index))
+    }
+  }
+}
