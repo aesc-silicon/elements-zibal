4e1d951f97e9dd7e4e1226e05fdc4111daf0757b
Daniel Schultz <dnltz@aesc-silicon.de>
2022-02-23 19:11:52 +0100

    hardware: helium1: Fix wrong DTS name
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/zibal/soc/Helium1/Helium1.scala
+++ hardware/scala/zibal/soc/Helium1/Helium1.scala
@@ -21,7 +21,7 @@ object Helium1 {
 
   def prepare(soc: Helium1, elementsConfig: ElementsConfig.ElementsConfig) {
     val dt = ZephyrTools.DeviceTree(elementsConfig)
-    dt.generate("hydrogen1.dtsi", "hydrogen", soc.system.axiCrossbar.slavesConfigs,
+    dt.generate("helium1.dtsi", "helium", soc.system.axiCrossbar.slavesConfigs,
                 soc.system.apbBridge.io.axi, soc.apbMapping, soc.irqMapping)
     val board = ZephyrTools.Board(elementsConfig)
     board.addLed("heartbeat", soc.peripherals.gpioStatusCtrl, 0)
