20951e2b6b71207d80faeb9d0cb43f0207c6858b
Daniel Schultz <d.schultz@phytec.de>
2023-11-15 21:43:02 +0100

    hardware: elements: sdk: Add 'duration' as argument
    Allow to call simulations and pass the duration as sbt argument.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- hardware/scala/elements/sdk/package.scala
+++ hardware/scala/elements/sdk/package.scala
@@ -6,5 +6,6 @@ package object sdk {
   trait ElementsApp extends App {
     val elementsConfig = ElementsConfig(this)
     lazy val simType = if (args.length < 1) "simulate" else args(0)
+    lazy val simDuration = if (args.length < 2) 10 else args(1)
   }
 }
