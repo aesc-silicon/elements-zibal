d5f482f652927589a80f3d520ed8ac1d36b65680
Daniel Schultz <d.schultz@phytec.de>
2024-02-18 11:33:22 +0100

    hardware: elements: Add Lithium1
    Lithium is a platform with a hyperbus interface which provides
    bigger sram for the firmware.
    
    It uses a VexRiscv without caches right now because they're not
    working. They should be added later for better performance.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ hardware/scala/elements/soc/Lithium1/Lithium1.scala
@@ -0,0 +1,48 @@
+package elements.soc
+
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.amba3.apb._
+import spinal.lib.bus.amba4.axi._
+
+import zibal.misc._
+import zibal.platform.Lithium
+import zibal.board.BoardParameter
+import zibal.soc.SocParameter
+
+import nafarr.peripherals.io.gpio.{Apb3Gpio, Gpio, GpioCtrl}
+import nafarr.peripherals.com.uart.{Apb3Uart, Uart, UartCtrl}
+import nafarr.peripherals.com.i2c.{Apb3I2cController, I2c, I2cCtrl, I2cControllerCtrl}
+import nafarr.peripherals.com.spi.{Apb3SpiController, Spi, SpiCtrl, SpiControllerCtrl}
+
+object Lithium1 {
+  def apply(parameter: Lithium.Parameter) = Lithium1(parameter)
+
+  case class Parameter(boardParameter: BoardParameter) extends SocParameter(boardParameter, 2) {
+    val uartStd = UartCtrl.Parameter.full
+    val gpioStatus = GpioCtrl.Parameter(Gpio.Parameter(4), 3, (0 to 2), (3 to 3), (3 to 3))
+  }
+
+  case class Lithium1(parameter: Lithium.Parameter) extends Lithium.Lithium(parameter) {
+    var socParameter = parameter.getSocParameter.asInstanceOf[Parameter]
+    val io_per = new Bundle {
+      val uartStd = master(Uart.Io(socParameter.uartStd))
+      val gpioStatus = Gpio.Io(socParameter.gpioStatus.io)
+    }
+
+    val peripherals = new ClockingArea(clockCtrl.getClockDomainByName("system")) {
+
+      val uartStdCtrl = Apb3Uart(socParameter.uartStd)
+      uartStdCtrl.io.uart <> io_per.uartStd
+      addApbDevice(uartStdCtrl.io.bus, 0x00000, 4 kB)
+      addInterrupt(uartStdCtrl.io.interrupt)
+
+      val gpioStatusCtrl = Apb3Gpio(socParameter.gpioStatus)
+      gpioStatusCtrl.io.gpio <> io_per.gpioStatus
+      addApbDevice(gpioStatusCtrl.io.bus, 0x1000, 4 kB)
+      addInterrupt(gpioStatusCtrl.io.interrupt)
+
+      connectPeripherals()
+    }
+  }
+}
