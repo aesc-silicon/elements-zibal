33a6b2847cfae3aa3aa1380e79145a98f580d46b
Daniel Schultz <d.schultz@phytec.de>
2024-04-27 21:54:07 -0700

    eda: Lattice: fpga: Create build dir
    This script fails when the fpga/ directory doesn't exist.
    Create one, in case it doesn't exist yet, before synthesizing
    the design.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- eda/Lattice/fpga/syn.sh
+++ eda/Lattice/fpga/syn.sh
@@ -6,6 +6,8 @@ TOP=${BOARD}Top
 
 cd ${ZIBAL_BUILD}
 
+mkdir -p ${FPGA_BUILD}
+
 yosys -p "read_verilog ${ZIBAL_BUILD}/${TOP}.v; synth_${FPGA_FAMILY} -top ${TOP} -json ${FPGA_BUILD}/${TOP}.json"
 nextpnr-${FPGA_FAMILY} -v --${FPGA_DEVICE} --package ${FPGA_PACKAGE} --json ${FPGA_BUILD}/${TOP}.json --lpf ${ZIBAL_BUILD}/${TOP}.lpf --textcfg ${FPGA_BUILD}/${TOP}.config --freq ${FPGA_FREQUENCY}
 ecppack ${FPGA_BUILD}/${TOP}.config ${FPGA_BUILD}/${TOP}.bit
