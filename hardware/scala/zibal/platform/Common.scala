14e897195117bbdf60fd303ff0d1131983579471
Daniel Schultz <dnltz@aesc-silicon.de>
2025-04-07 15:35:07 +0200

    hardware. zibal: platform: Common: Add Pinmux Helper
    Add helper functions required to generate pinmux tables, which
    can be passed to the pinmux controller.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/zibal/platform/Common.scala
+++ hardware/scala/zibal/platform/Common.scala
@@ -5,13 +5,17 @@ import spinal.lib._
 
 import zibal.soc.SocParameter
 
+import scala.collection.mutable.Map
 import scala.collection.mutable.ArrayBuffer
 
 import spinal.lib.bus.amba3.apb._
 import spinal.lib.bus.amba4.axi._
 import spinal.lib.bus.misc.SizeMapping
+import spinal.lib.io.{TriStateArray, TriState}
+import spinal.lib.io.ReadableOpenDrain
 
 import nafarr.system.plic.Apb3Plic
+import nafarr.peripherals.pinmux.Apb3Pinmux
 
 abstract class PlatformParameter(socParameter: SocParameter) {
   def getKitParameter = socParameter.getKitParameter
@@ -22,6 +26,8 @@ abstract class PlatformParameter(socParameter: SocParameter) {
 abstract class PlatformComponent(parameter: PlatformParameter) extends Component {
   val apbMapping = ArrayBuffer[(Apb3, SizeMapping)]()
   val irqMapping = ArrayBuffer[(Int, Bool)]()
+  val pinmuxInputs = Map[String, (Int, TriState[Bool])]()
+  val pinmuxMapping = ArrayBuffer[(Int, List[Int])]()
 
   var apbBridge: Axi4SharedToApb3Bridge = null
   var plicCtrl: Apb3Plic = null
@@ -50,6 +56,62 @@ abstract class PlatformComponent(parameter: PlatformParameter) extends Component
   def addInterrupt(pin: Bool) {
     irqMapping += nextInterruptNumber -> pin
     nextInterruptNumber += 1
+  }
+
+  def addPinmuxInput(pin: Bool, name: String, output: Boolean = true) {
+    assert(
+      !pinmuxInputs.contains(name),
+      s"Unable to assign input with name ${name}. Another input is already using this name."
+    )
+    val tmp = TriState(Bool)
+    if (output) {
+      tmp.writeEnable := True
+      tmp.write := pin
+    } else {
+      tmp.writeEnable := False
+      tmp.write := False
+      pin := tmp.read
+    }
+    pinmuxInputs += name -> (pinmuxInputs.size, tmp)
+  }
+
+  def addPinmuxInput(pin: TriState[Bool], name: String) {
+    assert(
+      !pinmuxInputs.contains(name),
+      s"Unable to assign input with name ${name}. Another input is already using this name."
+    )
+    val tmp = TriState(Bool)
+    pin.read := tmp.read
+    tmp.writeEnable := pin.writeEnable
+    tmp.write := pin.write
+    pinmuxInputs += name -> (pinmuxInputs.size, tmp)
+  }
+
+  def addPinmuxInput(pin: ReadableOpenDrain[Bool], name: String) {
+    assert(
+      !pinmuxInputs.contains(name),
+      s"Unable to assign input with name ${name}. Another input is already using this name."
+    )
+    val tmp = TriState(Bool)
+    pin.read := tmp.read
+    tmp.writeEnable := False
+    tmp.write := pin.write
+    pinmuxInputs += name -> (pinmuxInputs.size, tmp)
+  }
+
+  def addPinmuxOption(pin: Int, inputs: List[String]) {
+    val inputPins = for (input <- inputs) yield (pinmuxInputs(input)._1)
+    pinmuxMapping.append((pin, inputPins))
+  }
+
+  def getPinmuxMapping() = pinmuxMapping
+
+  def connectPinmuxInputs(pinmux: Apb3Pinmux) {
+    for ((key, (index, pin)) <- pinmuxInputs) {
+      pinmux.io.inputs(index).write := pin.write
+      pinmux.io.inputs(index).writeEnable := pin.writeEnable
+      pin.read := pinmux.io.inputs(index).read
+    }
   }
 
   def initOnChipRam(path: String)
