2145c410b59169b9d4638f4d6b78ab3059f7c4e5
Daniel Schultz <dnltz@aesc-silicon.de>
2026-04-24 20:19:03 +0200

    hardware: zibal: platform: Hydrogen: Switch to VexiiRiscv
    Switch from the VexRiscv to the VexiiRiscv CPU. This CPU
    configuration is optimized on area instead of speed and
    made to small microcontrollers.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/zibal/platform/nonmetals/Hydrogen.scala
+++ hardware/scala/zibal/platform/nonmetals/Hydrogen.scala
@@ -7,7 +7,6 @@ package zibal.platform
 import spinal.core._
 import spinal.lib._
 
-import zibal.cores.VexRiscvCoreParameter
 import zibal.soc.SocParameter
 import zibal.misc.BinTools
 import zibal.misc.BaremetalTools
@@ -24,10 +23,9 @@ import nafarr.system.reset.{WishboneResetController, ResetControllerCtrl}
 import nafarr.system.clock.{WishboneClockController, ClockControllerCtrl}
 import nafarr.memory.spi.{BmbSpiXipController}
 import nafarr.peripherals.com.spi.{Spi, SpiControllerCtrl}
-import spinal.lib.com.jtag.Jtag
+import nafarr.cores.cpu.vexiiriscv.{VexiiRiscvCoreParameter, VexiiRiscvBmb}
 
-import vexriscv._
-import vexriscv.plugin._
+import spinal.lib.com.jtag.Jtag
 
 object Hydrogen {
 
@@ -51,12 +49,13 @@ object Hydrogen {
           (ram, ram.io.bus)
         }
   ) extends PlatformParameter(socParameter) {
-    val core = VexRiscvCoreParameter.realtime(0xa0000000L).plugins
+    val core = VexiiRiscvCoreParameter.realtime(0xa0000000L)
     val mtimer = MachineTimerCtrl.Parameter.default
     val plic = PlicCtrl.Parameter.default(getSocParameter.getInterruptCount(0))
     val clocks = ClockControllerCtrl.Parameter(getKitParameter.clocks)
     val resets = ResetControllerCtrl.Parameter(getKitParameter.resets)
     val spi = SpiControllerCtrl.Parameter.xip()
+
   }
 
   class Hydrogen(parameter: Parameter) extends PlatformComponent(parameter) {
@@ -89,32 +88,20 @@ object Hydrogen {
     clockCtrl.io.mainClock := io_plat.clock
 
     val core = new ClockingArea(clockCtrl.getClockDomainByName("system")) {
-      val mtimerInterrupt = Bool
-      val globalInterrupt = Bool
-
-      val config = VexRiscvConfig(
-        plugins = parameter.core += new DebugPlugin(clockCtrl.getClockDomainByName("debug"))
+      val cpu = new VexiiRiscvBmb(
+        VexiiRiscvBmb.Parameter(
+          parameter.core.plugins,
+          parameter.core.iBusBmbParam,
+          parameter.core.dBusBmbParam
+        ),
+        clockCtrl.getClockDomainByName("debug")
       )
 
-      val cpu = new VexRiscv(config)
-      var iBus: Bmb = null
-      var dBus: Bmb = null
-      for (plugin <- config.plugins) plugin match {
-        case plugin: IBusSimplePlugin => iBus = plugin.iBus.toBmb()
-        case plugin: IBusCachedPlugin => iBus = plugin.iBus.toBmb()
-        case plugin: DBusSimplePlugin => dBus = plugin.dBus.toBmb()
-        case plugin: DBusCachedPlugin => dBus = plugin.dBus.toBmb()
-        case plugin: CsrPlugin => {
-          plugin.externalInterrupt := globalInterrupt
-          plugin.timerInterrupt := mtimerInterrupt
-        }
-        case plugin: DebugPlugin =>
-          clockCtrl.getClockDomainByName("debug") {
-            resetCtrl.triggerByNameWithCond("system", RegNext(plugin.io.resetOut))
-            io_plat.jtag <> plugin.io.bus.fromJtag()
-          }
-        case _ =>
+      clockCtrl.getClockDomainByName("debug") {
+        resetCtrl.triggerByNameWithCond("system", RegNext(cpu.ndmreset))
       }
+
+      io_plat.jtag <> cpu.jtag
     }
 
     val system = new ClockingArea(clockCtrl.getClockDomainByName("system")) {
@@ -163,25 +150,24 @@ object Hydrogen {
 
       /* Generate BMB Crossbar */
       val iBusDecoder = BmbDecoder(
-        p = VexRiscvCoreParameter.iBusConfig,
+        p = parameter.core.iBusBmbParam,
         mappings = Seq(onChipRam.mapping, spiXipController.mapping),
         capabilities = Seq(onChipRam.bmbParameter, spiXipController.bmbParameter)
       )
-      iBusDecoder.io.input << core.iBus.pipelined(
+      iBusDecoder.io.input << core.cpu.iBus.pipelined(
         cmdValid = true,
         cmdReady = true,
         rspValid = true,
         rspReady = true
       )
-      iBusDecoder.io.input.cmd.mask := B"1111"
 
       val dBusDecoder = BmbDecoder(
-        p = VexRiscvCoreParameter.dBusConfig,
+        p = parameter.core.dBusBmbParam,
         mappings = Seq(onChipRam.mapping, spiXipController.mapping, wishboneBridge.mapping),
         capabilities =
           Seq(onChipRam.bmbParameter, spiXipController.bmbParameter, wishboneBridge.bmbParameter)
       )
-      dBusDecoder.io.input << core.dBus.pipelined(
+      dBusDecoder.io.input << core.cpu.dBus.pipelined(
         cmdValid = true,
         cmdReady = true,
         rspValid = true,
@@ -189,7 +175,7 @@ object Hydrogen {
       )
 
       val onChipRamArbiter = BmbArbiter(
-        inputsParameter = Seq(VexRiscvCoreParameter.iBusConfig, VexRiscvCoreParameter.dBusConfig),
+        inputsParameter = Seq(parameter.core.iBusBmbParam, parameter.core.dBusBmbParam),
         outputParameter = onChipRam.bmbParameter,
         lowerFirstPriority = true
       )
@@ -198,7 +184,7 @@ object Hydrogen {
       onChipRamArbiter.io.inputs(1) << dBusDecoder.io.outputs(0)
 
       val spiXipControllerArbiter = BmbArbiter(
-        inputsParameter = Seq(VexRiscvCoreParameter.iBusConfig, VexRiscvCoreParameter.dBusConfig),
+        inputsParameter = Seq(parameter.core.iBusBmbParam, parameter.core.dBusBmbParam),
         outputParameter = spiXipController.bmbParameter,
         lowerFirstPriority = true
       )
@@ -210,11 +196,11 @@ object Hydrogen {
 
       /* Peripheral IP-Cores */
       val plicCtrl = WishbonePlic(parameter.plic, wishboneConfig)
-      core.globalInterrupt := plicCtrl.io.interrupt
+      core.cpu.globalInterrupt := plicCtrl.io.interrupt
       addPeripheralDevice(plicCtrl.io.bus, 0x800000, 4 MB)
 
       val mtimerCtrl = WishboneMachineTimer(parameter.mtimer, wishboneConfig)
-      core.mtimerInterrupt := mtimerCtrl.io.interrupt
+      core.cpu.mtimerInterrupt := mtimerCtrl.io.interrupt
       addPeripheralDevice(mtimerCtrl.io.bus, 0x20000, 4 kB)
 
       val resetCtrlMapper = WishboneResetController(parameter.resets, wishboneConfig)
