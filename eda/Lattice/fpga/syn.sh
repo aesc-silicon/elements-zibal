24d553f510e400741ab9d033988aa983d330e297
Daniel Schultz <dnltz@aesc-silicon.de>
2026-05-04 16:33:35 +0200

    Taskfile: Make sbt and FPGA flow verbose during CI
    CI currently hits a maximum output log error. Decrease the
    output log for sbt and the FPGA flow to save log space.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- eda/Lattice/fpga/syn.sh
+++ eda/Lattice/fpga/syn.sh
@@ -12,11 +12,25 @@ cd ${ZIBAL_BUILD}
 
 mkdir -p ${FPGA_BUILD}
 
+QUIET=0
+for arg in "$@"; do
+  case "$arg" in
+    --quiet) QUIET=1 ;;
+  esac
+done
+
 ADDITIONAL_VERILOG=""
 if [ -f ${ZIBAL_BUILD}/${TOP}Blackboxes.v ]; then
 	ADDITIONAL_VERILOG="read_verilog ${ZIBAL_BUILD}/${TOP}Blackboxes.v;"
 fi
 
-yosys -p "${ADDITIONAL_VERILOG} read_verilog ${ZIBAL_BUILD}/${TOP}.v; synth_${FPGA_FAMILY} -top ${TOP} -json ${FPGA_BUILD}/${TOP}.json"
-nextpnr-${FPGA_FAMILY} -v --${FPGA_DEVICE} --package ${FPGA_PACKAGE} --json ${FPGA_BUILD}/${TOP}.json --lpf ${ZIBAL_BUILD}/${TOP}.lpf --textcfg ${FPGA_BUILD}/${TOP}.config --freq ${FPGA_FREQUENCY}
+YOSYS_FLAGS=""
+NEXTPNR_FLAGS=""
+if [ "$QUIET" -eq 1 ]; then
+  YOSYS_FLAGS="-q"
+  NEXTPNR_FLAGS=""
+fi
+
+yosys ${YOSYS_FLAGS} -p "${ADDITIONAL_VERILOG} read_verilog ${ZIBAL_BUILD}/${TOP}.v; synth_${FPGA_FAMILY} -top ${TOP} -json ${FPGA_BUILD}/${TOP}.json"
+nextpnr-${FPGA_FAMILY} ${NEXTPNR_FLAGS} --${FPGA_DEVICE} --package ${FPGA_PACKAGE} --json ${FPGA_BUILD}/${TOP}.json --lpf ${ZIBAL_BUILD}/${TOP}.lpf --textcfg ${FPGA_BUILD}/${TOP}.config --freq ${FPGA_FREQUENCY}
 ecppack ${FPGA_BUILD}/${TOP}.config ${FPGA_BUILD}/${TOP}.bit
